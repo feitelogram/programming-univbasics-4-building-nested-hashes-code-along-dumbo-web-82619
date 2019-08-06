afb917b8ec356f24b94649d6948a96fcf8be79e1
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-06 19:31:16 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,19 +2,19 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-      :name => "Lord Montague", :age => "53"
+        :name => "Lord Montague", :age => "53"
         },
       :matriarch => {
         :name => "Lady Montague", :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo", :age => "15", :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        
         },
       :matriarch => {
         #your key/value pairs here
